2bc67662e955fb358a28a7a6e626390b80b65fe1
sammyatparachute <48567572+sammyatparachute@users.noreply.github.com>
2022-05-16 19:07:27 -0400

    Connected Mongoose

--- views/show.jsx
+++ views/show.jsx
@@ -2,19 +2,19 @@ const React = require('react')
 const Default = require('./layouts/Default')
 
 function Show ({bread, index}) {
-  //console.log(bread.name)
     return (
         <Default>
         <h3>{bread.name}</h3>
         <p>
-          and it
           {
             bread.hasGluten
-            ? <span> does </span>
-            : <span> does NOT </span>
+            ? <span> Has gluten </span>
+            : <span> Does NOT have gluten </span>
           }
-          have gluten.
         </p>
+        <p id="ingredient-list">Ingredients: {bread.ingredients}</p>
+        <script>let ingredientsList = document.getElementById("ingredient-list")</script>
+        
         <img src={bread.image} alt={bread.name} />
         <a href={`/breads/${index}/edit`}><button>Edit</button></a>
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
